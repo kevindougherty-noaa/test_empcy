228b654497336d58b63b996a9947dc7e73aef4c2
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-01-24 18:28:05 +0000

    first commit

---
+++ src/emcpy/calcs/units.py
@@ -0,0 +1,105 @@
+__all__ = ['K_to_C', 'K_to_F', 'C_to_K', 'C_to_F', 'F_to_C',
+           'mps_to_MPH', 'MPH_to_mps', 'mm_to_inches', 'inches_to_mm']
+
+
+# Temperature
+def K_to_C(K):
+    """
+    Convert Kelvin to Celsius
+    Args:
+        K : (float, or array of floats) temperature in kelvin
+    Returns:
+        The input temperature in degrees celsius
+    """
+    return K - 273.15
+
+
+def K_to_F(K):
+    """
+    Convert Kelvin to Fahrenheit
+    Args:
+        K : (float, or array of floats) temperature in kelvin
+    Returns:
+        The input temperature in degrees fahrenheit
+    """
+    return (K-273.15)*9/5.+32
+
+
+def C_to_K(T_C):
+    """
+    Convert Celsius to Kelvin
+    Args:
+        T_C : (float, or array of floats) temperature in degrees celsius
+    Returns:
+        The input temperature in kelvin
+    """
+    return T_C + 273.15
+
+
+def C_to_F(C):
+    """
+    Convert Celsius to Fahrenheit
+    Args:
+        C : (float, or array of floats) temperature in degrees celsius
+    Returns:
+        The input temperature in degrees fahrenheit
+    """
+    return C*9/5.+32
+
+
+def F_to_C(F):
+    """
+    Convert Fahrenheit to Celsius
+    Args:
+        F : (float, or array of floats) temperature in degrees fahrenheit
+    Returns:
+        The input temperature in degrees celsius
+    """
+    return (F-32) * 5/9
+
+
+# Wind
+def mps_to_MPH(mps):
+    """
+    Convert meters per second to miles per hour
+    Args:
+        mps : (float, or array of floats) speed in meters per second
+    Returns:
+        The input speed in miles per hour
+    """
+    return mps * 2.2369
+
+
+def MPH_to_mps(MPH):
+    """
+    Convert miles per hour to meters per second
+    Args:
+        MPH : (float, or array of floats) speed in miles per hour
+    Returns:
+        The input speed in meters per second
+    """
+    return MPH / 2.2369
+
+
+# Precipitation
+def mm_to_inches(mm):
+    """
+    Convert millimeters to inches
+    Args:
+        mm : (float, or array of floats) length in millimeters
+    Returns:
+        The input length in inches
+    """
+    return mm * 0.0394
+
+
+def inches_to_mm(inches):
+    """
+    Convert inches to millimeters
+    Args:
+        inches : (float, or array of floats) length in inches
+    Returns:
+        The input length in millimeters
+    """
+    return inches / 0.0394
+
---
+++ src/emcpy/calcs/wind.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+__all__ = ['uv_to_spddir', 'spddir_to_uv']
+
+
+def uv_to_spddir(u, v, direction=False):
+    """
+    Calculates the wind speed from u and v components.
+    Will calculate direction as well if parameter direction = True.
+    Takes into account the wind direction coordinates is different than
+    the trig unit circle coordinate. If the wind direction is 360,
+    then return zero.
+    Args:
+        u : (array like) u (easterly) wind component
+        v : (array like) v (northerly) wind component
+        direction: (bool, optional, default=False)
+    Returns:
+        An array of wind speed (and an array of wind direction if direction=True)
+    """
+    u = np.array(u)
+    v = np.array(v)
+
+    wdir = (270 - np.rad2deg(np.arctan2(v, u))) % 360
+    wspd = np.sqrt(u * u + v * v)
+
+    return wspd, wdir if direction else wspd
+
+
+def spddir_to_uv(wspd, wdir):
+    """
+    Calculate the u and v wind components from wind speed and direction.
+    Args:
+        wspd : (array like) wind speed
+        wdir : (array like) wind direction in degrees
+    Returns:
+        The u,v wind vector components.
+    """
+    wspd = np.array(wspd, dtype=float)
+    wdir = np.array(wdir, dtype=float)
+
+    rad = 4.0 * np.arctan(1) / 180.
+    u = -wspd * np.sin(rad * wdir)
+    v = -wspd * np.cos(rad * wdir)
+
+    # If the speed is zero, then u and v should be set to zero (not NaN)
+    if wspd == 0:
+        u = float(0)
+        v = float(0)
+
+    return u, v
+
