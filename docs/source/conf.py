05a492149b691dd12db1f1795d40d25a0750c73e
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-01-25 19:14:13 +0000

    updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,13 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    ".ipynb_checkpoints",
+    ".vscode",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
